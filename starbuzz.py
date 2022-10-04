38cc88fc411ce07892a0de3c99b738b56b963d04
Gabriela <gabedeandradee@gmail.com>
2022-10-04 15:51:04 -0300

    main definida

--- starbuzz.py
+++ starbuzz.py
@@ -1,13 +1,21 @@
 import urllib.request
 
-def lerUrl(url):
-    pagina = urllib.request.urlopen(url)
-    texto = pagina.read().decode("utf8")
-    return identificaValor(texto)
+def main():
+    def compra(url):
+        preco = lerUrl("http://beans.itcarlow.ie/prices-loyalty.html")
+        print(preco)
 
-def identificaValor(conteudo):
-    inicio = conteudo.find(">$") + 2
-    fim = conteudo.find('</', inicio)
-    return conteudo[inicio:fim]
+    def lerUrl(url):
+        pagina = urllib.request.urlopen(url)
+        texto = pagina.read().decode("utf8")
+        return identificaValor(texto)
 
-print(lerUrl("http://beans.itcarlow.ie/prices-loyalty.html"))
+    def identificaValor(conteudo):
+        inicio = conteudo.find(">$") + 2
+        fim = conteudo.find('</', inicio)
+        return conteudo[inicio:fim]
+
+    compra("http://beans.itcarlow.ie/prices-loyalty.html")
+
+if __name__ == '__main__':
+    main()
