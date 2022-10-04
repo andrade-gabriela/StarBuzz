f2434171a26312198de6bbb1d37f06756ff9dfd0
Gabriela <gabedeandradee@gmail.com>
2022-10-04 17:37:24 -0300

    funcao email modelo

--- starbuzz.py
+++ starbuzz.py
@@ -1,10 +1,18 @@
+# Gabriela -> github.com/andrade-gabriela
+# 04/10/2022
+
 import urllib.request
+import smtplib
+from email.mime.text import MIMEText
+
 
 def main():
+    # 
     def compra(url):
         preco = lerUrl("http://beans.itcarlow.ie/prices-loyalty.html")
         print(preco)
 
+    #
     def lerUrl(url):
         pagina = urllib.request.urlopen(url)
         texto = pagina.read().decode("utf8")
@@ -14,6 +22,24 @@ def main():
         inicio = conteudo.find(">$") + 2
         fim = conteudo.find('</', inicio)
         return conteudo[inicio:fim]
+
+    def enviaEmail():
+        remetente = '' #criar um email
+        destino = [''] #email de quem vai receber
+
+        port = 1025
+        msg = MIMEText('Texto do e-mail')
+
+        msg['Subject'] = 'Test e-mail'
+        msg['From'] = ''#email criado p/ remetente
+        msg['To'] = ''#email destino
+
+        with smtplib.SMTP('localhost', port) as server:
+
+            # server.login('username', 'password')
+            server.sendmail(remetente, destino, msg.as_string())
+            print("E-mail enviado com sucesso")
+
 
     compra("http://beans.itcarlow.ie/prices-loyalty.html")
 
